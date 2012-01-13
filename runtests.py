a3bb96d58126897390bcb4fabe36d7e2f3e4b3ba
Jason Webb <bigjasonwebb@gmail.com>
2012-01-13 10:09:47 -0700

    Version and year bump.

--- runtests.py
+++ runtests.py
@@ -4,18 +4,17 @@ from sys import stdout
 
 if __name__ == "__main__":
     disc_folder = path.abspath(path.dirname(__file__))
-    
+
     stdout.write("Discovering tests in '%s'..." % disc_folder)
     suite = unittest.TestSuite()
     loader = unittest.loader.defaultTestLoader
     suite.addTest(loader.discover(disc_folder, pattern="test*.py"))
     stdout.write("Done.\n")
-    
+
     runner = unittest.TextTestRunner()
     runner.verbosity = 2
-    
+
     print "Running tests..."
     print
-    
-    runner.run(suite.run)
 
+    runner.run(suite.run)
