fb14ee043fa3cd5e9e4af2fc62bced4234ebad89
David Hughes <d@vidhughes.com>
2013-02-08 12:52:50 -0800

    Changed version handling to fix pip install issues

--- djchoices/__init__.py
+++ djchoices/__init__.py
@@ -1,5 +1,11 @@
+import os
+from distutils.version import StrictVersion
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    v = StrictVersion(f.readlines()[0])
+    VERSION = v.version
+    __version__ = str(v)
+
 from djchoices.choices import ChoiceItem, DjangoChoices, C
 
 __all__ = ["ChoiceItem", "DjangoChoices", "C"]
-
-VERSION = (1, 1, 9)
--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from os import path
 from setuptools import setup, find_packages
+from distutils.version import StrictVersion
 
-from djchoices import VERSION
+with open(path.join(path.dirname(__file__), 'djchoices/VERSION')) as f:
+    VERSION = StrictVersion(f.readlines()[0])
 
 with open(path.join(path.dirname(__file__), 'README.rst')) as f:
     readme = f.read()
 
 setup(
     name="django-choices",
-    version=".".join(map(str, VERSION)),
+    version=str(VERSION),
     license="MIT",
     description="Sanity for the django choices functionality.",
     long_description=readme,
