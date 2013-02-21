d0a5342ddd405cddb8b67bf426bdafbe8f6f93c9
Jason Webb <bigjasonwebb@gmail.com>
2013-02-21 15:10:16 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(path.dirname(__file__), 'README.rst')) as f:
 
 setup(
     name="django-choices",
-    version="1.1.10",
+    version="1.1.11",
     license="MIT",
     description="Sanity for the django choices functionality.",
     long_description=readme,
