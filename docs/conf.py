29d2da094c3c5059c952dc219f9d1de3e3ff96f5
Sergei Maertens <sergei@maykinmedia.nl>
2017-01-28 13:39:54 +0100

    Add docs to test matrix

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,7 @@ pygments_style = 'sphinx'
 
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
+suppress_warnings = ['image.nonlocal_uri']
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -137,7 +138,7 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
