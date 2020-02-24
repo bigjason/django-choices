df7019d60ebba8519d93c8a578214d55638d1f63
Adam Johnson <me@adamj.eu>
2020-02-24 11:23:13 +0000

    Document and test Django 3.0 support
    Finish the work from #69, with a changelog note, documentation update, tox and travis grid additions, and PyPI classifier.

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Framework :: Django :: 2.0",
         "Framework :: Django :: 2.1",
         "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
